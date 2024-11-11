c066fe934f6e30e1228c3d995a8c1a4ad9ad1725
Hezekiah Munene <hezemunene01@gmail.com>
2024-11-11 12:06:00 +0300

    resolves spacing

--- lib/myhomepage.dart
+++ lib/myhomepage.dart
@@ -9,7 +9,6 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
@@ -26,12 +25,17 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: const Center( 
+      body: const Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            TextWidget(displayText: 'Welcome to bakery heaven, where flour meets prefession', styleVariant: 'title'),
-            TextWidget(displayText: 'Sharing divine sweetness', styleVariant: 'subtitle'),
+            TextWidget(
+                displayText:
+                    'Welcome to bakery heaven, where flour meets prefession',
+                styleVariant: 'title'),
+            TextWidget(
+                displayText: 'Sharing divine sweetness',
+                styleVariant: 'subtitle'),
             TextWidget(displayText: 'Get started here', styleVariant: 'normal')
           ],
         ),
--- lib/widgets/text.dart
+++ lib/widgets/text.dart
@@ -1,34 +1,26 @@
 import 'package:flutter/material.dart';
 
 class TextWidget extends StatelessWidget {
-
-  const TextWidget({
-    super.key, 
-    required this.displayText, 
-    required this.styleVariant
-  });
+  const TextWidget(
+      {super.key, required this.displayText, required this.styleVariant});
 
   final String displayText;
   final String styleVariant; // title, subtitle, normal-text,small-text
-  
-  
+
   @override
   Widget build(BuildContext context) {
-    getStyleVariant (String styleVariant){ 
-    switch (styleVariant) {
-      case 'title':
-        return const TextStyle(fontWeight: FontWeight.bold, fontSize: 30);
-      case 'subtitle':
-        return const TextStyle(fontWeight: FontWeight.normal, fontSize: 25);
-  
-      default:
-        return const TextStyle(fontSize: 20);
-    }
-  }
-     return  Text(displayText, style: getStyleVariant(styleVariant));
+    getStyleVariant(String styleVariant) {
+      switch (styleVariant) {
+        case 'title':
+          return const TextStyle(fontWeight: FontWeight.bold, fontSize: 30);
+        case 'subtitle':
+          return const TextStyle(fontWeight: FontWeight.normal, fontSize: 25);
 
-    //  style: (styleVariant == 'title') ? const TextStyle(fontWeight: FontWeight.bold, fontSize: 30)
-    //   : (styleVariant == 'subtitle')? const TextStyle(fontWeight: FontWeight.normal, fontSize: 25) : const TextStyle(fontSize: 20));
+        default:
+          return const TextStyle(fontSize: 20);
+      }
+    }
+
+    return Text(displayText, style: getStyleVariant(styleVariant));
   }
 }
-
